46e3289379fbc5dc4418ab387f2e766dbd4d0516
Matheus Garrido <matheusgarrido10@hotmail.com>
2022-01-15 17:47:28 -0300

    feat: create redux types to footer

--- src/constants/reduxTypes.js
+++ src/constants/reduxTypes.js
@@ -1,5 +1,9 @@
 const reduxTypes = {
   changeLesson: "CHANGE_LESSON",
+  firstLesson: "FIRST_LESSON",
+  lastLesson: "LAST_LESSON",
+  previousLesson: "PREVIOUS_LESSON",
+  nextLesson: "NEXT_LESSON",
 };
 
 export default reduxTypes;
