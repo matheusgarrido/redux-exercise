c7ded2307f0bafcb4240f02c27c9bb3d267fd51a
Matheus Garrido <matheusgarrido10@hotmail.com>
2022-01-15 16:04:38 -0300

    feat: print modules and lessons list

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,13 +1,32 @@
+import { connect } from "react-redux";
 import React from "react";
 import "./style.scss";
 
-const Main = () => {
+const Main = ({ modules, currentModule, currentLession }) => {
+  console.log(modules, currentModule, currentLession);
   return (
     <main className="main">
-      Aulas
-      <div></div>
+      <h1>Programação Web </h1>
+      {modules.map((module) => (
+        <div key={module.id}>
+          {module.title}
+          <ul>
+            {module.lessons.map((lesson) => (
+              <li key={lesson.id} className="main__lesson">
+                {lesson.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </main>
   );
 };
 
-export default Main;
+const mapStateToProps = (state) => ({
+  modules: state.modules,
+  currentModule: state.currentModule,
+  currentLession: state.currentLession,
+});
+
+export default connect(mapStateToProps)(Main);
