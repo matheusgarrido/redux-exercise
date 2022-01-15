9af20a8866e59d3155f449dc06742cb8466be927
Matheus Garrido <matheusgarrido10@hotmail.com>
2022-01-15 17:46:49 -0300

    feat: footer style

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul className="buttons">
+        <li>Primeira Aula</li>
+        <li>Voltar Aula</li>
+        <li>Próxima Aula</li>
+        <li>Última Aula</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
