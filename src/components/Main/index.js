c6b99638ca1d9d233277abd0eba18c623bfaea75
Matheus Garrido <matheusgarrido10@hotmail.com>
2022-01-15 15:47:51 -0300

    feat: main component

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.scss";
+
+const Main = () => {
+  return (
+    <main className="main">
+      Aulas
+      <div></div>
+    </main>
+  );
+};
+
+export default Main;
